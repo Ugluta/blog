a9a16f518e8a2b4e98b539c67a91534f32bb47ae
Ugluta <101148103+Ugluta@users.noreply.github.com>
2026-06-24 08:00:37 +0300

    config: move egitimportal to port 3002 (3000 in use by bet app)

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: 'egitimportal',
     script: 'node_modules/.bin/next',
-    args: 'start',
+    args: 'start -p 3002',
     cwd: './',
     instances: 1,
     autorestart: true,
@@ -10,7 +10,7 @@ module.exports = {
     max_memory_restart: '512M',
     env: {
       NODE_ENV: 'production',
-      PORT: 3000,
+      PORT: 3002,
     },
   }],
 }
